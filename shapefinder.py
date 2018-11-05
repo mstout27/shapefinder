d4a1b4a3d454a45fe8ee00d2ece0bb4cd45153ca
mstout27 <mstout@udel.edu>
2018-11-05 11:14:34 -0500

    added documentation

--- shapefinder.py
+++ shapefinder.py
@@ -14,7 +14,8 @@ args = parser.parse_args()
 red = 0
 green = 255
 blue = 0
-# determine correct output based on flags
+
+# if -c flag, read hex color input
 if args.color[0:2] == "0x":
     red = args.color[2:4]
     red = int(red,16)
@@ -22,6 +23,7 @@ if args.color[0:2] == "0x":
     green = int(green,16)
     blue = args.color[6:8]
     blue = int(blue,16)
+# if -s flag, read word color input
 if args.color:
     if args.color == "red":
         red = 255
@@ -56,6 +58,7 @@ if args.color:
         green == 255
         blue == 255
 
+# read shape input
 sides = 0
 if args.shape:
     if args.shape == "triangle":
@@ -71,37 +74,39 @@ if args.shape:
 # load the image, convert it to grayscale, and blur it
 image = cv2.imread("lee.PPM")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (3, 3), 0)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
+#display blurred and grayed image
 cv2.imshow("Gray",gray)
 cv2.waitKey(0)
 
-# detect edges in the image
-edged = cv2.Canny(gray, 10, 75)
+# detect edges in image
+edges = cv2.Canny(gray, 10, 75)
 
-cv2.imshow("Edged", edged)
+#show edges image
+cv2.imshow("Edges", edges)
 cv2.waitKey(0)
 
-# construct and apply a closing kernel to 'close' gaps between 'white'
-# pixels
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (4, 4))
-closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+# kernal for closing edges
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
 
+# show closed image
 cv2.imshow("Closed", closed)
 cv2.waitKey(0)
 
-# find contours (i.e. the 'outlines') in the image and initialize the
-# total number of books found
-_, cnts, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# retrieve contours from fixed image
+_, cnts, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 total = 0
 
-# loop over the contours
+# loop over contours
 for c in cnts:
-    # approximate the contour
+
+    # approximate contour
     peri = cv2.arcLength(c, True)
     approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 
-    # if the approximated contour has four points, then rectangle
+    # Use contour & user input to determine shape to detect
     if len(approx) == 3 and sides == 3:
         cv2.drawContours(image, [approx], -1, (red, green, blue), 4)
         total +=1
@@ -115,12 +120,8 @@ for c in cnts:
         cv2.drawContours(image, [approx], -1, (red, green, blue), 4)
         total += 1
 
-
-
-       
-
-# display the output
-print ("I found {0} books in that image".format(total))
+# Output
+print ("Found {0} {}s in image".format(total, args.shape))
 cv2.imshow("Output", image)
 
 # if -o flag, save output to file
